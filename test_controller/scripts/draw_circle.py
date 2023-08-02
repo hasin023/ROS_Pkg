cc8d7bc8a02399705b69542180ef8145f68bd7ce
Hasin Mahtab <hasinmahtab.alvee@gmail.com>
2023-08-02 02:17:11 -0700

    Made Draw_Cricle publisher

--- test_controller/scripts/draw_circle.py
+++ test_controller/scripts/draw_circle.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python3
+
+import rospy
+from geometry_msgs.msg import Twist
+
+if __name__ == '__main__':
+    rospy.init_node('draw_circle')
+    rospy.loginfo("Started drawing circle")
+
+    publisher = rospy.Publisher("/turtle1/cmd_vel", Twist, queue_size=10)
+
+    rate = rospy.Rate(2)
+
+    while not rospy.is_shutdown():
+        message = Twist()
+        message.linear.x = 3.0
+        message.angular.z = 1.0
+        publisher.publish(message)
+        rate.sleep()
