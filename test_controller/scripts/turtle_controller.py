f84ca0d236b9c50be31bc5fad8ab30fc2e95895d
Hasin Mahtab <hasinmahtab.alvee@gmail.com>
2023-08-02 23:17:11 -0700

    Added color to basic controller

--- test_controller/scripts/turtle_controller.py
+++ test_controller/scripts/turtle_controller.py
@@ -7,6 +7,8 @@ from geometry_msgs.msg import Twist
 from turtlesim.msg import Pose
 from turtlesim.srv import SetPen
 
+previous_x = 0
+
 
 def call_set_pen_service(r, g, b, width, off):
     rospy.wait_for_service("/turtle1/set_pen")
@@ -40,7 +42,6 @@ def process_instructions(instructions):
             rate.sleep()
 
         elif i == 'L':
-
             for x in range(0, 10):
                 call_set_pen_service(255, 0, 0, 5, 0)
                 rospy.logwarn("Turning Left")
@@ -48,7 +49,6 @@ def process_instructions(instructions):
                 rate.sleep()
 
         elif i == 'R':
-
             for x in range(0, 10):
                 call_set_pen_service(0, 255, 0, 5, 0)
                 rospy.logwarn("Turning Right")
@@ -69,6 +69,15 @@ def avoid_border(data):
         avoid_cmd.angular.z = 0.0
 
     publisher.publish(avoid_cmd)
+
+    global previous_x
+    if data.x >= 5.5 and previous_x < 5.5:
+        rospy.logwarn("Crossed the middle line")
+        call_set_pen_service(0, 255, 0, 5, 0)
+    elif data.x <= 5.5 and previous_x > 5.5:
+        rospy.logwarn("Crossed the middle line")
+        call_set_pen_service(255, 0, 0, 5, 0)
+    previous_x = data.x
 
 
 def draw_square():
